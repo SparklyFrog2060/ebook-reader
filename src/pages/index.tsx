fcb4437bb0d5c88d9d1525788915f7991f40e7d2
SparklyFrog2060 <150049334+SparklyFrog2060@users.noreply.github.com>
2025-02-13 20:38:38 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
-  return EbookReader (
+  return (
 
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
