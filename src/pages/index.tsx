25cbe748c6b320a4d6c476f7885fc111eb225eb4
SparklyFrog2060 <150049334+SparklyFrog2060@users.noreply.github.com>
2025-02-13 20:47:49 +0100

    Final Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-var
-// eslint-disable-next-line
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Image from "next/image";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Geist, Geist_Mono } from "next/font/google";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import EbookReader from '../components/EbookReader';
 
 const geistSans = Geist({
@@ -16,7 +17,6 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
@@ -114,6 +114,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-    
   );
 }
