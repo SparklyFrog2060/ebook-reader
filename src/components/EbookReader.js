6215347fedab85b4ee6bb56197a25b53863a17bd
SparklyFrog2060 <150049334+SparklyFrog2060@users.noreply.github.com>
2025-02-13 20:34:07 +0100

    Update EbookReader.js

--- src/components/EbookReader.js
+++ src/components/EbookReader.js
@@ -1,4 +1,4 @@
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
   import React, { useState, useRef } from 'react';
   import { Card, CardContent } from '@/components/ui/card';
   import { Button } from '@/components/ui/button';
